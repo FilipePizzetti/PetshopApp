6b5f479c91f6c50e61c40387aab2d2ea487ea317
Filipe Pizzetti <pizzetti.filipe@gmail.com>
2025-02-17 18:27:35 -0300

    primeira parte petshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            NavbarComponent,
+            LoginPageComponent,
+            ResetPasswordPageComponent,
+            SignupPageComponent,
+            PetsPageComponent,
+            ProductsPageComponent,
+            CartPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
